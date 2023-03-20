d03f19353aea953a3fab43f991d2a0f440f76b00
moyuyyyy <127403964+moyuyyyy@users.noreply.github.com>
2023-03-20 20:28:25 +0800

    Create direction.h

---
+++ direction.h
@@ -0,0 +1,10 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+
+void up(int (*arr)[4],int* num);
+void down(int (*arr)[4],int* num);
+void left(int (*arr)[4],int* num);
+void right(int (*arr)[4],int* num);
+
+#endif//DIRECTION_H
