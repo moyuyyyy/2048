6bd2eda848aa9fc61ec77d4bce062e7be716fdc2
moyuyyyy <127403964+moyuyyyy@users.noreply.github.com>
2023-03-20 20:28:58 +0800

    Create tools.h

---
+++ tools.h
@@ -0,0 +1,9 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+void rand_arr(int (*arr)[4]);
+void rand_two(int (*arr)[4]);
+void show(int (*arr)[4],int* num);
+extern bool flag;
+extern int (*arr)[4];
+#endif//TOOLS_H
